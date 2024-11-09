4676ec5394a5edd510dc5c65d3a460a8d0ca52a9
Yurii Mironov <yuriymironov96@gmail.com>
2024-11-09 23:44:59 +0200

    feat: navbar interactivity

--- mironov-blog-2024-frontend/src/components/Navbar.tsx
+++ mironov-blog-2024-frontend/src/components/Navbar.tsx
@@ -1,13 +1,20 @@
+'use client'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Navbar() {
+  const pathname = usePathname()
+
   return (
     <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
       <div className="flex items-center gap-4">
-        <a href="/" className="font-bold text-xl">
-          Mironov Blog
-        </a>
-        <a href="/about">About</a>
+        <Link href="/" className={pathname === "/" ? "font-bold text-xl" : ""}>Mironov Blog</Link>
+        <Link href="/about" className={pathname === "/about" ? "font-bold text-xl" : ""}>
+          About
+        </Link>
       </div>
       <div className="flex items-center gap-4">
+        {/* those will be outside links */}
         <a href="/login">Login</a>
         <a href="/register">Register</a>
       </div>
